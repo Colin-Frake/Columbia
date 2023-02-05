6c5edfd5a26405d2c446e1c511bfdd6fa47b39e3
Colin-Frake <63562169+Colin-Frake@users.noreply.github.com>
2023-02-04 23:17:21 -0500

    Add files via upload

---
+++ Assignement1.R
@@ -0,0 +1,35 @@
+# R Assignment 1
+
+
+# Create a variable 'x' that holds a value of 9
+x=9
+print(x)
+# Create a variable 'y' that holds a value of x
+y=x
+# Create a variable 'z' that holds the sum of x and y
+z=y+x
+# Clear the variable x from the workspace
+rm(x)
+# Clear all variables from the workspace
+rm(y)
+rm(z)
+# Make a statement that directs to http://www.google.com
+print("https://google.com")
+# Show a list of all packages installed
+print(installed.packages())
+# Show a list of packages currently installed and usable
+
+
+
+# Magic Number Exercise
+# Do the Following:
+# Choose any number and add 2 to it.
+# Multiply the result by 3.
+# Subtract 6 from the answer.
+# Divide what you get by 3.
+# you should end up with the same number that you started with
+x=2
+x=x+2
+x=x*3
+x=x-6
+x=x/3
